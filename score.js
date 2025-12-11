a748cbdc9a7d923b41fbedd53a6e167fc5bbd139
viven836 <eeshakarpuradu@gmail.com>
2025-12-11 19:34:18 +0530

    2

--- score.js
+++ score.js
@@ -4,10 +4,7 @@ let gscore = 0
 document.getElementById("hscore-el").textContent = hscore
 document.getElementById("gscore-el").textContent = gscore
 
-function add1() {
-    hscore = hscore + 1
-    document.getElementById("hscore-el").textContent = hscore
-}
+
 
 function add1() {
     hscore = hscore + 1
@@ -27,4 +24,24 @@ function add3() {
 function reset() {
     hscore = 0
     document.getElementById("hscore-el").textContent = hscore
+}
+
+function add11() {
+    gscore = gscore + 1
+    document.getElementById("gscore-el").textContent = gscore
+}
+
+function add22() {
+    gscore = gscore + 2
+    document.getElementById("gscore-el").textContent = gscore
+}
+
+function add33() {
+    gscore = gscore + 3
+    document.getElementById("gscore-el").textContent = gscore
+}
+
+function reset1() {
+    gscore = 0
+    document.getElementById("gscore-el").textContent = gscore
 }

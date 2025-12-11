241f011111a4fa779a8f507a77c524a8face6e7b
viven836 <eeshakarpuradu@gmail.com>
2025-12-11 19:29:31 +0530

    1

---
+++ score.js
@@ -0,0 +1,30 @@
+let hscore = 0
+let gscore = 0
+
+document.getElementById("hscore-el").textContent = hscore
+document.getElementById("gscore-el").textContent = gscore
+
+function add1() {
+    hscore = hscore + 1
+    document.getElementById("hscore-el").textContent = hscore
+}
+
+function add1() {
+    hscore = hscore + 1
+    document.getElementById("hscore-el").textContent = hscore
+}
+
+function add2() {
+    hscore = hscore + 2
+    document.getElementById("hscore-el").textContent = hscore
+}
+
+function add3() {
+    hscore = hscore + 3
+    document.getElementById("hscore-el").textContent = hscore
+}
+
+function reset() {
+    hscore = 0
+    document.getElementById("hscore-el").textContent = hscore
+}
